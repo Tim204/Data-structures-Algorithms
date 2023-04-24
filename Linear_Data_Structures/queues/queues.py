70db6abbfc5c5fa39c91491c73be4421db75020a
Timoteo Tati <timoteo14@live.com.pt>
2023-04-25 00:23:11 +0200

    Refactored queue implementation

--- Linear_Data_Structures/queues/queues.py
+++ Linear_Data_Structures/queues/queues.py
@@ -7,21 +7,27 @@ class QueueStack:
         self._first_stack = Stack()
         self._second_stack = Stack()
 
+    def _reverse_first_stack(self):
+        if self._second_stack.is_empty():
+            while not self._first_stack.is_empty():
+                self._second_stack.push(self._first_stack.pop())
+
     def enqueue(self, data):
         self._first_stack.push(data)
 
     def dequeue(self):
-        if self._second_stack.is_empty():
-            while not self._first_stack.is_empty():
-                self._second_stack.push(self._first_stack.pop())
+        if self.is_empty():
+            raise Exception("The stack is empty")
+        self._reverse_first_stack()
         return self._second_stack.pop()
 
     def peek(self):
+        if self.is_empty():
+            raise Exception("The stack is empty")
+        self._reverse_first_stack()
         return self._second_stack.peek()
 
     def is_empty(self):
-        return self._first_stack.is_empty()
+        return self._first_stack.is_empty() and self._second_stack.is_empty()
 
-    def is_full(self):
-        return not self._first_stack.is_empty()
 
