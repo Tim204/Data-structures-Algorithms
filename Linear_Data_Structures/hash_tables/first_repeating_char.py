aa67f013149d93eaea8af68f7e439056596650f6
Timoteo Tati <timoteo14@live.com.pt>
2023-04-29 18:12:32 +0200

    Added method to check first reapeting character

--- Linear_Data_Structures/hash_tables/first_repeating_char.py
+++ Linear_Data_Structures/hash_tables/first_repeating_char.py
@@ -1,21 +1,33 @@
-""" Finding the First Non-Repeating Character on a string using
-    Hash-tables(Dictionaries)"""
+"""
+    Finding Repeating Character on a string using
+    Hash-tables(Dictionaries) and sets
+"""
 
 
 class CharFinder:
     def __init__(self):
         self._dict = {}
 
-    def non_repeating_chars(self, string):
-        for chars in string.lower():
-            if chars.isalpha():
-                self._dict.__setitem__(chars, string.lower().count(chars))
+    def first_non_repeating_chars(self, string):
+        for char in string.lower():
+            if char.isalpha():
+                self._dict.__setitem__(char, string.lower().count(char))
 
         for key, value in self._dict.items():
             if value == 1:
                 return key
-        return 0
+        return None
+
+    def first_repeating_char(self, string):
+        """For getting the first repeating char a set can be used instead of a dict"""
+        self._dict = {None}  # Passing None to the dict as key to trick the interpreter to think it's a set
+        for char in string.lower():
+            if self._dict.__contains__(char):
+                return char
+            self._dict.add(char.lower())
+        return None
 
 
-ch = CharFinder()
-print(ch.non_repeating_chars("I Live off Grid"))
+find = CharFinder()
+print(find.first_non_repeating_chars("Hello world!"))
+print(find.first_repeating_char("Hello world!"))
