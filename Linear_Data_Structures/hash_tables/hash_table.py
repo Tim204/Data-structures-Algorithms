098aa6a4ab8aea48266b99dc8feacb4154ebbfc3
Timoteo Tati <timoteo14@live.com.pt>
2023-05-01 00:31:26 +0200

    Hash Tables finilized

--- Linear_Data_Structures/hash_tables/hash_table.py
+++ Linear_Data_Structures/hash_tables/hash_table.py
@@ -20,6 +20,28 @@ class HashTable:
         self._size = size
         self._length = 0
 
+    def put(self, key, value):
+        self.__setitem__(key, value)
+
+    def get(self, key):
+        try:
+            return self.__getitem__(key)
+        except KeyError:
+            return None
+
+    def remove(self, key):
+        self.__delitem__(key)
+
+    def keys(self):
+        return self.__iter__()
+
+    def values(self):
+        for key_var in self._key_val_iterator():
+            yield key_var[1]
+
+    def items(self):
+        return self._key_val_iterator()
+
     def __setitem__(self, key, value):
         for list_element in self._storage[self._compute_index(key)]:
             if key == list_element[0]:  # already exist, update its value
@@ -29,43 +51,24 @@ class HashTable:
             self._storage[self._compute_index(key)].append([key, value])
             self._length += 1
 
-    def put(self, key, value):
-        self.__setitem__(key, value)
-
-    def _compute_index(self, key):
-        return hash(key) % self._size
-
     def __getitem__(self, key):
-        """
-        get by key, if not found, raise key error
-        :raise: KeyError
-        :return: value
-        """
-        storage_idx = hash(key) % self._size
-        for ele in self._storage[storage_idx]:
-            if ele[0] == key:
-                return ele[1]
+        for list_element in self._storage[self._compute_index(key)]:
+            if list_element[0] == key:
+                return list_element[1]
 
-        raise KeyError('Key {} dont exist'.format(key))
+        raise KeyError(f'Key {key} does not exist')
 
     def __delitem__(self, key):
-        """
-        delete key value from current dictionary instance
-        :param key: str
-        :return: None
-        """
-        storage_idx = hash(key) % self._size
-        for sub_lst in self._storage[storage_idx]:
+        for sub_lst in self._storage[self._compute_index(key)]:
             if key == sub_lst[0]:
-                self._storage[storage_idx].remove(sub_lst)
+                self._storage[self._compute_index(key)].remove(sub_lst)
                 self._length -= 1
                 return
 
-        raise KeyError('Key {} dont exist'.format(key))
+        raise KeyError(f'Key "{key}" does not exist')
 
     def __contains__(self, key):
-        storage_index = hash(key) % self._size
-        for item in self._storage[storage_index]:
+        for item in self._storage[self._compute_index(key)]:
             if item[0] == key:
                 return True
         return False
@@ -73,95 +76,48 @@ class HashTable:
     def __len__(self):
         return self._length
 
-    def __iterate_kv(self):
-        """
-        return an iterator
-        :return: generator
-        """
+    def __iter__(self):
+        for key_var in self._key_val_iterator():
+            yield key_var[0]
+
+    def _key_val_iterator(self):
         for sub_lst in self._storage:
             if not sub_lst:
                 continue
             for item in sub_lst:
                 yield item
 
-    def __iter__(self):
-        """
-        return an iterator
-        :return: generator
-        """
-        for key_var in self.__iterate_kv():
-            yield key_var[0]
-
-    def keys(self):
-        """
-        get all keys as list
-        :return: list
-        """
-        return self.__iter__()
-
-    def values(self):
-        """
-        get all values as list
-        :return: list
-        """
-        for key_var in self.__iterate_kv():
-            yield key_var[1]
-
-    def items(self):
-        """
-        get all k:v as list
-        :return: list
-        """
-        return self.__iterate_kv()
-
-    def get(self, key):
-        """
-        get value by key
-        :param key: str
-        :return: value
-        """
-        try:
-            return self.__getitem__(key)
-        except KeyError:
-            return None
+    def _compute_index(self, key):
+        return hash(key) % self._size
 
     def __str__(self):
-        """
-        str representation of the dictionary
-        :return: string
-        """
         res = []
         for ele in self._storage:
             for key_value in ele:
                 if isinstance(key_value[0], str):
-                    key_str = '\'{}\''.format(key_value[0])
+                    key_str = f'\'{key_value[0]}\''
                 else:
-                    key_str = '{}'.format(key_value[0])
+                    key_str = f'{key_value[0]}'
                 if isinstance(key_value[1], str):
-                    value_str = '\'{}\''.format(key_value[1])
+                    value_str = f'\'{key_value[1]}\''
                 else:
-                    value_str = '{}'.format(key_value[1])
+                    value_str = f'{key_value[1]}'
 
-                res.append('{}: {}'.format(key_str, value_str))
+                res.append(f'{key_str}: {value_str}')
         key_value_pairs_str = '{}'.format(', '.join(res))
         return '{' + key_value_pairs_str + '}'
 
     def __repr__(self):
-        """
-        string representation of the class instances
-        :return: string
-        """
         return self.__str__()
 
 
 dic = HashTable()
-dic[1] = 2
-dic[3] = "Anna"
-dic["age"] = 10
-dic.__setitem__("available", True)
-
+dic["name"] = "John"
+dic.put("age", 20)
+dic.remove("age")
 
 print(dic)
+
 
 
 
