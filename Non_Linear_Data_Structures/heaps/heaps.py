d13d1542c8269b1bf67bcafaff55b757bb3af51d
Timoteo Tati <timoteo14@live.com.pt>
2023-05-21 23:59:34 +0200

    Implemented remove method

--- Non_Linear_Data_Structures/heaps/heaps.py
+++ Non_Linear_Data_Structures/heaps/heaps.py
@@ -17,8 +17,62 @@ class Heap:
         self.size += 1
         self._bubble_up()
 
+    def remove(self):
+        if self.is_empty():
+            raise Exception("Can't remove items from an empty heap")
+
+        self.items[0] = self.items[self.size - 1]
+        self.size -= 1
+        index = 0
+
+        while index <= self.size and not self._parent_is_valid(index):
+            higher_child_index = self._higher_child_index(index)
+            self._swap(index, higher_child_index)
+            index = higher_child_index
+
+    def _higher_child_index(self, index):
+        if not self._has_left_child(index):
+            return index
+
+        if not self._has_right_child(index):
+            return self._left_child_index(index)
+
+        return self._left_child(index) \
+            if self._left_child_index(index) > self._right_child_index(index) \
+            else self._right_child_index(index)
+
+    def _has_left_child(self, index):
+        return self._left_child_index(index) <= self.size
+
+    def _has_right_child(self, index):
+        return self._right_child_index(index)
+
+    def _parent_is_valid(self, index):
+        if not self._has_left_child(index):
+            return True
+        if not self._has_right_child(index):
+            return self.items[index] >= self._left_child(index)
+
+        return self.items[index] >= self._left_child(index) \
+            and self.items[index] >= self._right_child(index)
+
+    def _left_child(self, index):
+        return self.items[self._left_child_index(index)]
+
+    def _right_child(self, index):
+        return self.items[self._right_child_index(index)]
+
+    def _left_child_index(self, index):
+        return index * 2 + 1
+
+    def _right_child_index(self, index):
+        return index * 2 + 2
+
     def is_full(self):
         return self.size == self._limit
+
+    def is_empty(self):
+        return self.size == 0
 
     def _bubble_up(self):
         """Moving up the big values to satisfy the heap property"""
@@ -42,5 +96,7 @@ heap.insert(5)
 heap.insert(17)
 heap.insert(4)
 heap.insert(22)
+heap.remove()
+
 
 print(heap.items)
