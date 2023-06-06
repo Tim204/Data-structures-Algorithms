5585c5d3f13d3a59ae36b91e52566c7b561dfd28
Timoteo Tati <timoteo14@live.com.pt>
2023-06-06 23:42:56 +0200

    Traversing a trie recursively

--- Non_Linear_Data_Structures/tries/trie.py
+++ Non_Linear_Data_Structures/tries/trie.py
@@ -1,29 +1,70 @@
-class Node:
 
-    def __init__(self, value=None, length=26):
-        self._value = value
-        self._children = [length]
+class TrieNode:
+
+    def __init__(self, value=None):
+        self.value = value
+        self._children = {}
         self._is_end = False
 
+    def has_child(self, ch):
+        return self._children.__contains__(ch)
+
+    def add_child(self, ch):
+        self._children.__setitem__(ch, TrieNode(ch))
+
+    def get_child(self, ch):
+        return self._children.get(ch)
+
+    def get_children(self):
+        return self._children.values()
+
     def __str__(self):
-        return f"value={self._value}"
+        return f"value={self.value}"
+
+    def __repr__(self):
+        return self.__str__()
 
 
-class Trie(Node):
+class Trie(TrieNode):
     def __init__(self):
         super().__init__()
-        self._root = Node(' ')
+        self._root = TrieNode(' ')
 
     def inset(self, word):
         current = self._root
         for ch in word:
-            index = ord(ch) - ord("a")
-
-
+            if not current.has_child(ch):
+                current.add_child(ch)
+            current = current.get_child(ch)
         current._is_end = True
+
+    def contains(self, word):
+        if word is None:
+            return False
+
+        current = self._root
+        for ch in word:
+            if not current.has_child(ch):
+                return False
+            current = current.get_child(ch)
+        return current._is_end
+
+    def traverse(self):
+        self._traverse(self._root)
+
+    def _traverse(self, root):
+        # PRE-ORDER-TRAVERSAL:  Visit the root first
+        print(root.value)
+        for child in root.get_children():
+            self._traverse(child)
+
+        # POST-ORDER: Simply visit the children first
+        # for child in root.get_children():
+        #     self._traverse(child)
+        # print(root.value)
 
 
 trie = Trie()
-trie.inset("cat")
-trie.inset("can")
-print("Done")
+trie.inset("compare")
+trie.traverse()
+print(trie.contains(None))
